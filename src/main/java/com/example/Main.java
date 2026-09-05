64ffca5182f835218289bd83c880bb4566e876a6
unknown <bambalbi23@yandex.ru>
2026-09-05 13:57:44 +0300

    feat: add console user interface

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,71 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+
+            printMenu();
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("Команда должна быть числом.");
+                scanner.nextLine();
+                System.out.print("Выберите действие: ");
+            }
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("Создание пользователя");
+                    break;
+
+                case 2:
+                    System.out.println("Поиск пользователя по ID");
+                    break;
+
+                case 3:
+                    System.out.println("Список пользователей");
+                    break;
+
+                case 4:
+                    System.out.println("Обновление пользователя");
+                    break;
+
+                case 5:
+                    System.out.println("Удаление пользователя");
+                    break;
+
+                case 0:
+                    running = false;
+                    System.out.println("Программа завершена.");
+                    break;
+
+                default:
+                    System.out.println("Неизвестная команда.");
+            }
         }
+
+        scanner.close();
+    }
+
+    private static void printMenu() {
+        System.out.println("USER SERVICE");
+        System.out.println("1. Создать пользователя");
+        System.out.println("2. Найти пользователя по ID");
+        System.out.println("3. Показать всех пользователей");
+        System.out.println("4. Обновить пользователя");
+        System.out.println("5. Удалить пользователя");
+        System.out.println("0. Выход");
+        System.out.print("Выберите действие: ");
     }
 }
+
