7eb16aa21fc538dedc600caf1e7f3fbaaaf51078
Movesha <movesha@gmail.com>
2026-09-11 17:50:52 +0300

    feat: add UserService and switch console

--- src/main/java/com/example/dao/UserDaoImpl.java
+++ src/main/java/com/example/dao/UserDaoImpl.java
@@ -22,7 +22,15 @@ public class UserDaoImpl implements UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDaoImpl.class);
 
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl() {
+        this(HibernateUtil.getSessionFactory());
+    }
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
     /** {@inheritDoc} */
     @Override
     public User save(User user) {
